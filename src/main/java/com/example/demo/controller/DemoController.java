ffa2d4c6eb030170af9ec09adc320820cae02eec
apollo <pingspong@outlook.com>
2023-05-09 10:44:53 +0800

    test controller

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,2 +1,18 @@
-package com.example.demo.controller;public class DemoController {
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author apollo
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/test")
+    public String testDemo(){
+        return "test demo success!";
+    }
 }
